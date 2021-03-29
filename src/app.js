a95a475ba8ae9177e572db6111878dc9afc5bdda
Cynthia <csleal694@gmail.com>
2021-03-28 22:17:53 -0500

    Matt x2

--- src/app.js
+++ src/app.js
@@ -1,1 +1,5 @@
-alert("Wednesday is too far away :'(")
+function booty() {
+    alert("Wednesday is too far away :'(");
+}
+let bootyButton = document.querySelector("button");
+bootyButton.addEventListener("click", booty);
