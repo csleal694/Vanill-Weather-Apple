99af221cd511265b86ee23222fab41446eaf4a42
Cynthia <csleal694@gmail.com>
2021-03-28 22:20:04 -0500

    Matt x3

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 function booty() {
     alert("Wednesday is too far away :'(");
+    alert("You're sexy 😘");
+    alert("Good night papi!");
 }
 let bootyButton = document.querySelector("button");
 bootyButton.addEventListener("click", booty);
