2ae68003644f9a770d462fb473834096025d8ee8
Cynthia <csleal694@gmail.com>
2021-03-29 21:00:23 -0500

    added HTML structure

--- src/app.js
+++
@@ -1,7 +0,0 @@
-function booty() {
-    alert("Wednesday is too far away :'(");
-    alert("You're sexy 😘");
-    alert("Good night papi!");
-}
-let bootyButton = document.querySelector("button");
-bootyButton.addEventListener("click", booty);
