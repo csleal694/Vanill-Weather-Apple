674c730f9858e1223d22a3bea39045f5b9125c5d
Cynthia <csleal694@gmail.com>
2021-04-12 10:38:32 -0500

    rearranged html order

--- src/app.js
+++ src/app.js
@@ -4,9 +4,9 @@ let descriptionElement = document.querySelector("#description");
 let humidityElement = document.querySelector("#humidity");
 let windElement = document.querySelector("#wind");
 let dateElement = document.querySelector("#date");
-let iconElement = document.getElementById("#icon");
+let iconElement = document.getElementById("icon");
 let precipitationElement = document.querySelector("#precipitation");
-
+let celsiusTemperature = null;
 
 function formatDate(timestamp) {
     let date = new Date(timestamp);
@@ -19,7 +19,15 @@ function formatDate(timestamp) {
         minutes = `0${minutes}`;
     }
 
-    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+    ];
     let day = days[date.getDay()];
     return `${day} ${hours}: ${minutes}`;
 }
@@ -46,7 +54,9 @@ function displayForecast(response) {
         <div class="col-2">
         <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
         
-        <img src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" 
+        <img src="http://openweathermap.org/img/wn/${
+          forecastDay.weather[0].icon
+        }@2x.png" 
         alt="" width="40" 
         />
             <div class="weather-forecast-temperatures">
@@ -67,26 +77,24 @@ function displayForecast(response) {
 function getForecast(coordinates) {
     console.log(coordinates);
     let apiKey = "93fec6aa4a6cd077a8e7a75e3382aae3";
-    let apiUrl = `https: //api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(displayForecast);
 }
 
 function displayTemperature(response) {
+    celsiusTemperature = response.data.main.temp;
 
-
-    celciusTemperture = response.data.main.temp;
-
-    temperatureElement.innerHTML = Math.round(celciusTemperture);
+    temperatureElement.innerHTML = Math.round(celsiusTemperature);
     cityElement.innerHTML = response.data.name;
     descriptionElement.innerHTML = response.data.weather[0].description;
     humidityElement.innerHTML = response.data.main.humidity;
     windElement.innerHTML = Math.round(response.data.wind.speed);
     // precipitationElement.innerHTML = response.data.main.precipitation;
     dateElement.innerHTML = formatDate(response.data.dt * 1000);
-    console.log(`
-    http: //openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`)
-    iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
-    iconElement.setAttribute("alt", response.data.weather[0].description);
+    iconElement.setAttribute(
+        "src",
+        `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    );
 
     getForecast(response.data.coord);
 }
@@ -101,7 +109,6 @@ function handleSubmit(event) {
     event.preventDefault();
     let cityInputElement = document.querySelector("#city-input");
     search(cityInputElement.value);
-
 }
 
 function displayFahrenheitTemperature(event) {
@@ -110,7 +117,7 @@ function displayFahrenheitTemperature(event) {
 
     celsiusLink.classList.remove("active");
     fahrenheitLink.classList.add("active");
-    let fahrenheitTemperature = (celciusTemperture * 9 / 5) + 32;
+    let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
     temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
 }
 
@@ -121,9 +128,6 @@ function displayCelsiusTemperature(event) {
     let temperatureElement = document.querySelector("#temperature");
     temperatureElement.innerHTML = Math.round(celsiusTemperature);
 }
-
-let celsiusTemperature = null;
-
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
